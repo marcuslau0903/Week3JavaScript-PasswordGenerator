c5abae67696e902582339a7b27fc35c4fa708a5d
MarcusLau <marcuslau0903@gmail.com>
2021-03-16 23:42:29 +0000

    Final submission

--- homework/Develop/script.js
+++ homework/Develop/script.js
@@ -5,7 +5,8 @@ const lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
 const upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const numbersArray = [1,2,3,4,5,6,7,8,9,0]
 const specialCharactersArray = ['!','£','%','^','&','*','(',')','_','+']
-const optionsArray = []
+let optionsArray = []
+let password =""
 
 const generatePassword = function() {
 
@@ -22,32 +23,44 @@ const generatePassword = function() {
     const isNumbers = confirm("Do you want Numbers?")
     const isSpecialCharacters = confirm("Do you want Special Characters?")
     
-   
+   // push and apply arrays into new variable "optionsArray" 
     if(isLowercase) {
-      optionsArray.push(lowerCaseArray)
+      optionsArray.push.apply(optionsArray,lowerCaseArray)
     } 
     if (isUppercase) {
-      optionsArray.push(upperCaseArray)
+      optionsArray.push.apply(optionsArray,upperCaseArray)
     }
     if (isNumbers) {
-      optionsArray.push(numbersArray)
+      optionsArray.push.apply(optionsArray,numbersArray)
     } 
     if (isSpecialCharacters)
-      optionsArray.push(specialCharactersArray)
+      optionsArray.push.apply(optionsArray,specialCharactersArray)
     }
     
-    // for(var i = 0; i < passwordLength; i++) {
-     //     //how to concat two strings together ??
-    //      how to generate random elements from optionsArray??
+    // Durstenfeld shuffle algorithm (googled)
+    //function declaration 
+    function shuffleArray(array) {
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+  }
+    // how to generate random elements from optionsArray??
+    for(let i = 0; i < passwordLength; i++) {
+    shuffleArray(optionsArray)
+    //selects a random index based on length of the options array
+    let randomIndex = Math.floor(Math.random()*(optionsArray.length)) 
+    //push randomly generated values based on user options into password.
+    //addition assignment operator (adding random characters to the password string)
+    password += (optionsArray[randomIndex])
+    shuffleArray(password)
     }
 
-
-     return optionsArray
+     return password
 
   }
-
-
-
 
 
 // Write password to the #password input
